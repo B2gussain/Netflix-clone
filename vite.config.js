cc8d619e0e6bb2230d5f4f3d067aa1b753df4435
B2gussain <bittuggussain@gmail.com>
2024-10-09 12:59:20 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,10 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [react()], // Correctly included in the return object
+    server: {
+      port: process.env.PORT || 3000, // fallback to port 3000 if no PORT env variable is set
+      host: true,
+  },
     define: {
       __API_KEY__: JSON.stringify(env.VITE_API_KEY), // Use the environment variable here
     },
